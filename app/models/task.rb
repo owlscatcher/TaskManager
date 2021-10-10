133f58e92bcf87fd5e5971870d9c726083ed2900
Alexandr Veselov <collabsis.g@gmail.com>
2021-10-10 20:32:41 +0400

    Added state machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,35 @@ class Task < ApplicationRecord
   belongs_to :assignee, class_name: 'User', optional: true
 
   validates :name, :description, :author, presence: true
-  validates :description, length: { maximum: 500 }
+  validates :description, presence: true, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :begin_develop do
+      transition new_task: :in_development
+    end
+
+    event :begin_test do
+      transition in_development: :in_qa
+    end
+
+    event :return_to_development do
+      transition [:in_qa, :in_code_review] => :in_development
+    end
+
+    event :start_review do
+      transition in_qa: :in_code_review
+    end
+
+    event :prepare_for_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :release do
+      transition ready_for_release: :released
+    end
+
+    event :archive do
+      transition [:new_task, :released] => :archived
+    end
+  end
 end
