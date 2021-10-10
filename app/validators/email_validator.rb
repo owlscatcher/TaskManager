b23d93426debd37bbb235be35ca0295c2201cc61
Alexandr Veselov <collabsis.g@gmail.com>
2021-10-10 20:26:04 +0400

    Fixed e-mail validation

--- app/validators/email_validator.rb
+++
@@ -1,7 +0,0 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A\S+@\S+\z/i
-      record.errors[attribute] << (options[:message] || 'is not an email')
-    end
-  end
-end
