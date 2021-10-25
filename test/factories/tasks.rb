b78b76519846ea757060da718f7fd62612cdd658
Alexandr Veselov <collabsis.g@gmail.com>
2021-10-25 15:27:13 +0400

    Fix state-machine in factories

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     description
     author { create :manager }
     assignee { create :developer }
-    state
+    traits_for_enum(:state, ['new_task', 'in_development', 'in_qa', 'in_code_review', 'ready_for_release', 'released', 'archived'])
     expired_at
   end
 end
