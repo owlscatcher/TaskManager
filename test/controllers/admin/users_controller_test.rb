6fa2ab65beb5fb8c7697a0b89b4684ecc07bc63b
Alexandr Veselov <collabsis.g@gmail.com>
2021-10-26 14:49:28 +0400

    Add list users

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -5,7 +5,12 @@ class Admin::UsersControllerTest < ActionController::TestCase
     admin = create(:admin)
     sign_in admin
   end
-  
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
   test "should get show" do
     user = create(:user)
     get :show, params: { id: user.id }
