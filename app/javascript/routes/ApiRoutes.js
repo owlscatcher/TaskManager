9ab8b55635dc27b1036460b651465a0a7f0c3963
Alexandr Veselov <collabsis.g@gmail.com>
2021-11-30 20:48:49 +0400

    Update ApiRoutes

--- app/javascript/routes/ApiRoutes.js
+++ app/javascript/routes/ApiRoutes.js
@@ -541,6 +541,9 @@ export const apiV1TaskPath = __jsr.r({"id":{"r":true},"format":{}}, [2,[7,"/"],[
  */
 export const apiV1TasksPath = __jsr.r({"format":{}}, [2,[7,"/"],[2,[6,"api"],[2,[7,"/"],[2,[6,"v1"],[2,[7,"/"],[2,[6,"tasks"],[1,[2,[8,"."],[3,"format"]]]]]]]]]);
 
+export const apiV1UsersPath = __jsr.r({"format":{}}, [2,[7,"/"],[2,[6,"api"],[2,[7,"/"],[2,[6,"v1"],[2,[7,"/"],[2,[6,"users"],[1,[2,[8,"."],[3,"format"]]]]]]]]]);
+
+
 /**
  * Generates rails route to
  * /board(.:format)
