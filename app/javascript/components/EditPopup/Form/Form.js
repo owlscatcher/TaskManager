b2203c794176bfffaa37fa0747a99e7a528c2a2b
Alexandr Veselov <collabsis.g@gmail.com>
2021-11-30 20:48:20 +0400

    Add UserSelect component

--- app/javascript/components/EditPopup/Form/Form.js
+++ app/javascript/components/EditPopup/Form/Form.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import { has } from 'ramda';
 
 import TextField from '@material-ui/core/TextField';
+import UserSelect from 'components/UserSelect';
 
 import useStyles from './useStyles';
 
 const Form = ({ errors, onChange, task }) => {
   const handleChangeTextField = (fieldName) => (event) => onChange({ ...task, [fieldName]: event.target.value });
+  const handleChangeSelect = (fieldName) => (user) => onChange({ ...task, [fieldName]: user });
   const styles = useStyles();
 
   return (
@@ -30,6 +32,15 @@ const Form = ({ errors, onChange, task }) => {
         required
         multiline
         margin="dense"
+      />
+      <UserSelect
+        label="Author"
+        value={task.author}
+        onChange={handleChangeSelect('author')}
+        isDisabled
+        isRequired
+        error={has('author', errors)}
+        helperText={errors.author}
       />
     </form>
   );
